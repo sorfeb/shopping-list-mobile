7d663e10c4d0de87a17f72cc78322f359b4c572a
sorfeb <soros21febriano@gmail.com>
2023-11-15 15:32:29 +0700

    Tutorial 8: Flutter Networking, Authentication, and Integration

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/screens/list_product.dart';
 import 'package:shopping_list/screens/menu.dart';
 import 'package:shopping_list/screens/shoplist_form.dart';
 
@@ -63,6 +64,18 @@ class LeftDrawer extends StatelessWidget {
                     builder: (context) => const ShopFormPage(),
                   ));
             },
+          ),
+
+          ListTile(
+              leading: const Icon(Icons.shopping_basket),
+              title: const Text('Daftar Produk'),
+              onTap: () {
+                  // Route menu ke halaman produk
+                  Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ProductPage()),
+                  );
+              },
           ),
         ],
       ),
